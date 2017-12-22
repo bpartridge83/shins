d45130fe6a4d045c5f2fddc97ff36d4b527e57ae
Brian Partridge <bpartridge83@gmail.com>
2017-12-21 21:43:16 -0800

    Adding graphql option to highlightjs

--- index.js
+++ index.js
@@ -211,6 +211,42 @@ function render(inputStr, options, callback) {
         var sh = hljs.getLanguage('bash');
         hljs.registerLanguage('shell', function (hljs) { return sh; });
         hljs.registerLanguage('sh', function (hljs) { return sh; });
+        hljs.registerLanguage('graphql', function (hljs) {
+          return {
+            aliases: ['gql'],
+            keywords: {
+              keyword: 'query mutation subscription|10 type interface union scalar fragment|10 enum on ...',
+              literal: 'true false null'
+            },
+            contains: [
+              hljs.HASH_COMMENT_MODE,
+              hljs.QUOTE_STRING_MODE,
+              hljs.NUMBER_MODE,
+              { className: 'type',
+                begin: '[^\\w][A-Z][a-z]', end: '\\W',
+                excludeEnd: true
+              },
+              { className: 'literal',
+                begin: '[^\\w][A-Z][A-Z]', end: '\\W',
+                excludeEnd: true
+              },
+              { className: 'variable',
+                begin: '\\$', end: '\\W',
+                excludeEnd: true
+              },
+              {
+                className: 'keyword',
+                begin: '[.]{2}', end: '\\.',
+              },
+              {
+                className: 'meta',
+                begin: '@', end: '\\W',
+                excludeEnd: true
+              }
+            ],
+            illegal: /([;<']|BEGIN)/
+          }
+        });
 
         while (inputArr.length<3) inputArr.push('');
 
